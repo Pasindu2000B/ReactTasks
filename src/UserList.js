e4db6c2e0397b667075b8296607f6fd4ceabb529
Pasindu2000B <pasindupremasirilk@gmail.com>
2024-10-08 14:48:41 +0530

    userLisSeconf  Commit

--- src/UserList.js
+++ src/UserList.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function UserList() {
+  let UserList = [
+    { name: "saman", age: 23 },
+    { name: "samantha", age: 25 },
+    { name: "shanaka", age: 21 },
+  ];
+
+  const [users, setUsers] = useState(UserList);
+
   return (
     <div>
       <h1>User List</h1>
+      <td>
+        {users.map((u, i) => {
+          return (
+            <>
+              <tr key={i}>{u.name}</tr>
+              <tr key={i}>{u.age}</tr>
+              <button
+                onClick={() => {
+                  setUsers([...users.slice(0, i), ...users.slice(i + 1)]);
+                }}
+              >
+                Delete
+              </button>
+            </>
+          );
+        })}
+      </td>
     </div>
   );
 }
